eee74937c589a3bdd91f7b007ec60f7c6d17f6c6
Patrick Tluszcz <patricktluszcz@gmail.com>
2019-08-12 02:39:39 +0200

    rebuilded

--- dist/common/BitmexAuth.d.ts
+++ dist/common/BitmexAuth.d.ts
@@ -9,13 +9,13 @@ interface IAuthHeaders {
     };
 }
 export declare function getAuthHeaders({ apiKeyID, apiKeySecret, opts, method, path }: IAuthHeaders): {
-    'api-expires'?: undefined;
-    'api-key'?: undefined;
-    'api-signature'?: undefined;
+    "api-expires"?: undefined;
+    "api-key"?: undefined;
+    "api-signature"?: undefined;
 } | {
-    'api-expires': number;
-    'api-key': string;
-    'api-signature': string;
+    "api-expires": number;
+    "api-key": string;
+    "api-signature": string;
 };
 export declare function getWSAuthQuery(apiKeyID: string, apiKeySecret: string): string;
 export {};
--- dist/common/BitmexAuth.js
+++ dist/common/BitmexAuth.js
@@ -10,25 +10,29 @@ function getAuthHeaders({ apiKeyID, apiKeySecret, opts, method, path }) {
         return {};
     }
     if (opts.qs) {
-        path += '?' + querystring_1.stringify(opts.qs);
+        path += "?" + querystring_1.stringify(opts.qs);
     }
-    const data = opts.form ? querystring_1.stringify(opts.form) : '';
+    const data = opts.form ? querystring_1.stringify(opts.form) : "";
     const nonce = generateNonce();
-    const signature = crypto_1.createHmac('sha256', apiKeySecret).update(method + path + nonce + data).digest('hex');
+    const signature = crypto_1.createHmac("sha256", apiKeySecret)
+        .update(method + path.substring(path.indexOf("/api")) + nonce + data)
+        .digest("hex");
     return {
-        'api-expires': nonce,
-        'api-key': apiKeyID,
-        'api-signature': signature
+        "api-expires": nonce,
+        "api-key": apiKeyID,
+        "api-signature": signature
     };
 }
 exports.getAuthHeaders = getAuthHeaders;
 function getWSAuthQuery(apiKeyID, apiKeySecret) {
     const nonce = generateNonce();
-    const signature = crypto_1.createHmac('sha256', apiKeySecret).update('GET/realtime' + nonce).digest('hex');
+    const signature = crypto_1.createHmac("sha256", apiKeySecret)
+        .update("GET/realtime" + nonce)
+        .digest("hex");
     return querystring_1.stringify({
-        'api-nonce': nonce,
-        'api-key': apiKeyID,
-        'api-signature': signature
+        "api-nonce": nonce,
+        "api-key": apiKeyID,
+        "api-signature": signature
     });
 }
 exports.getWSAuthQuery = getWSAuthQuery;
